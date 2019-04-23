4c8262e2222cd0dd6a05f564e908d02300155a64
Wambita Fana <wambitafana@gmail.com>
2019-04-23 09:24:03 +0300

    Add view function for index html

---
+++ app/main/views.py
@@ -0,0 +1,8 @@
+from flask import render_template,request,redirect,url_for
+from . import main
+
+@main.route('/')
+def index():
+
+    title = 'Home Page - Get The latest Pitch stories'
+    return render_template('index.html',title = title)
